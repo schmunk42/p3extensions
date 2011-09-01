30175c1a584c1a6e07ac6af3b21fb2fe4f0508ae
Tobias Munk <schmunk@usrbin.de>
2011-09-01 05:23:58 -0700

    fixed meta-data relation attribute

--- behaviors/P3MetaDataBehavior.php
+++ behaviors/P3MetaDataBehavior.php
@@ -1,12 +1,29 @@
 <?php
 
 class P3MetaDataBehavior extends CActiveRecordBehavior {
-
+	
+	public $metaDataRelation;
+	
+	private function getMetaDataRelation(){
+		if (!$this->metaDataRelation) {
+			// auto-find
+			$class = get_class($this->owner);
+			$metaDataRelation = strtolower($class[0]).substr($class,1).'Meta';
+			return $this->owner->$metaDataRelation;
+		} elseif ($this->metaDataRelation == "_self_") {
+			// special case for meta data tables
+			return $this->owner;
+		} else {
+			// manual setting
+			return $this->owner->{$this->metaDataRelation};
+		}
+	}
+	
 	public function beforeDelete($event) {
 		parent::beforeDelete($event);
-		if ($this->contentMeta->checkAccessWrite) {
-			if (Yii::app()->user->checkAccess($this->contentMeta->checkAccessWrite) === false) {
-				throw new CHttpException(403, "You are not authorized to perform this action.");
+		if ($this->getMetaDataRelation()->checkAccessDelete) {
+			if (Yii::app()->user->checkAccess($this->getMetaDataRelation()->checkAccessDelete) === false) {
+				throw new CHttpException(403, "You are not authorized to perform this action. Access restricted by P3MetaDataBehavior.");
 				return false;
 			} else {
 				
@@ -17,9 +34,9 @@ class P3MetaDataBehavior extends CActiveRecordBehavior {
 
 	public function beforeSave($event) {
 		parent::beforeSave($event);
-		if ($this->contentMeta->checkAccessWrite) {
-			if (Yii::app()->user->checkAccess($this->contentMeta->checkAccessWrite) === false) {
-				throw new CHttpException(403, "You are not authorized to perform this action.");
+		if ($this->getMetaDataRelation()->checkAccessUpdate) {
+			if (Yii::app()->user->checkAccess($this->getMetaDataRelation()->checkAccessUpdate) === false) {
+				throw new CHttpException(403, "You are not authorized to perform this action. Access restricted by P3MetaDataBehavior.");
 				return false;
 			}
 		}
